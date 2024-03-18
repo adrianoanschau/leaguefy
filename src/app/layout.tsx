8f61e85a4d66cd4eca3a7e919646f877b09febd4
Adriano Anschau <adrianoanschau@gmail.com>
2024-03-18 15:57:44 -0300

    add adsense account verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ import { SoccerDataContextProvider, ThemeContextProvider } from "@/contexts";
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-8804111254804343"
+      ></meta>
       <ThemeContextProvider>
         <SoccerDataContextProvider>
           <RootPage>{children}</RootPage>
